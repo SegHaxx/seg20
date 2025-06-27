10ea6aeae8f44462a79cc1f1e21a451a4b3eaddb
Seg <seg@haxxed.com>
2025-06-27 00:57:08 -0500

    fix line wrap on atari

--- i8080_tests.c
+++ i8080_tests.c
@@ -139,6 +139,9 @@ static inline void run_test(
 
 int main(void) {
   i8080 cpu;
+#ifdef __MINT__
+  Cconws("\33v");
+#endif
   run_test(&cpu, "tests/TST8080.COM", 4924LU);
   run_test(&cpu, "tests/8080PRE.COM", 7817LU);
   run_test(&cpu, "tests/CPUTEST.COM", 255653383LU);
